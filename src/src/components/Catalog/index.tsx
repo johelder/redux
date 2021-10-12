e5ee1eb519bdc4eb8b8d56a7b7a421f5825d82e6
johelder <johelder.arj@gmail.com>
2021-10-12 13:55:20 -0300

    listing catalog

--- src/src/components/Catalog/index.tsx
+++ src/src/components/Catalog/index.tsx
@@ -1,14 +1,35 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+import { IProduct } from "../../store/modules/cart/types";
 
 export function Catalog(){
 
-  const state = useSelector(state => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(state)
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get('products');
+      const data = response.data as IProduct[];
+      setCatalog(data);
+    }
+
+    getProducts();
+  }, []);
 
   return (
-    <div>
+    <main>
+
       <h1>Catalog</h1>
-    </div>
+
+      {catalog.map(product => (
+        <article>
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   );
 }
