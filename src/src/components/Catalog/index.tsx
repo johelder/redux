509bf60efa1ffaf0dbf6147943255005a249123e
johelder <johelder.arj@gmail.com>
2021-10-13 22:28:14 -0300

    rendering cart redux

--- src/src/components/Catalog/index.tsx
+++ src/src/components/Catalog/index.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import api from "../../services/api";
+import { addProductToCart } from "../../store/modules/cart/actions";
 
 import { IProduct } from "../../store/modules/cart/types";
 
 export function Catalog(){
-
   const [catalog, setCatalog] = useState<IProduct[]>([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function getProducts() {
@@ -17,17 +19,21 @@ export function Catalog(){
     getProducts();
   }, []);
 
+  const handleAddProductTocart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch]);
+
   return (
     <main>
 
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article>
-          <strong>{product.title}</strong>
-          <span>{product.price}</span>
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {" "}
 
-          <button type="button">Comprar</button>
+          <button type="button" onClick={() => handleAddProductTocart(product)}>Comprar</button>
         </article>
       ))}
     </main>
